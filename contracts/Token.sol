3a4a4bfb8be9609b5d16f85fbaf2199150387fb6
Dimitar Trepechov <dimitar.trepechov@mobisystems.com>
2021-12-02 18:34:29 +0200

    Add hard Hardhat exmaple contract

---
+++ contracts/Token.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.8.0;
+
+contract Token {
+    string public name = "My Hardhat Token";
+    string public symbol = "MHT";
+
+    uint256 public totalSupply = 1000000;
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        // The totalSupply is assigned to transaction sender, which is the account
+        // that is deploying the contract.
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    /**
+     * A function to transfer tokens.
+     *
+     * The `external` modifier makes a function *only* callable from outside
+     * the contract.
+     */
+    function transfer(address to, uint256 amount) external {
+        // Check if the transaction sender has enough tokens.
+        // If `require`'s first argument evaluates to `false` then the
+        // transaction will revert.
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        // Transfer the amount.
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
